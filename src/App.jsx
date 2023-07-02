26f5fc1e66493666d46d0640830572e091112729
Samuel Brito <sambriton7@gmail.com>
2023-07-01 21:37:26 -0300

    Creating and configuring CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import axios from "axios"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  //const url = `https://api.openweathermap.org/data/2.5/weather?lat=40.730610&lon=-73.935242&appid=c6aea05284b5751879f1f6303012ac54`
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="container" >
+        <div className="top">
+          <div className="location" >
+            <p>New York</p>
+          </div>
+          <div className="temp" >
+            <h1>60°F</h1>
+          </div>
+          <div className="description" >
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom" >
+          <div className="fells" >
+            <p>65°F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="winds">
+            <p>100km</p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
 
